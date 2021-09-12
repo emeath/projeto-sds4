a6816249982763fa9c4d22febe80e6bc9b86488f
Matheus M. Jeronimo <matheus.martins.jeronimo@gmail.com>
2021-09-12 14:48:00 -0300

    Fix useEffect errors

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -49,7 +49,6 @@ const BarChart = () => {
                         }
                     ]
                 });
-                console.log(chartData)
             })
     }, []);
 
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -21,7 +21,7 @@ const DataTable = () => {
             .then(response => {
                 setPage(response.data)
             })
-    }, [])
+    }, [activePage])
 
     const changePage = (index: number) => {
         setActivePage(index);
